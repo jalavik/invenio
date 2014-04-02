006a7cec67213b2aba553dce8fa05ef5d8d269ed
Guillaume Lastecoueres <PX9e@gmx.fr>
2014-04-02 11:11:46 +0200

    workflows: global improvement
    * Updates workflow to comply with the modification in
      tasks librairies.
    
    * Fixes an error with the new ObjectVersion type with
      python3.
    
    * Fixes sphinx mistakes.
    
    * Removes useless regex.
    
    * Fixes PEP8 mistakes.
    
    * Adds more documentation to some functions.
    
    * Removes useless and obsolete code, factorize some parts.
    
    * Changes the start workflow function to give the
      possibility to start from a new clean BibWorkflowObject.
    
    * Adds the refresh frequency as a parameter for workflows
      synchronisation.
    
    Signed-off-by: Guillaume Lastecoueres <guillaume.lastecoueres@cern.ch>

--- invenio/modules/workflows/engine.py
+++ invenio/modules/workflows/engine.py
@@ -24,7 +24,6 @@ from six.moves import cPickle
 from six import iteritems, reraise
 from uuid import uuid1 as new_uuid
 
-
 import base64
 
 from workflow.engine import (GenericWorkflowEngine,
@@ -33,16 +32,20 @@ from workflow.engine import (GenericWorkflowEngine,
                              StopProcessing,
                              JumpTokenBack,
                              JumpTokenForward,
-                             WorkflowError)
+                             WorkflowError,
+                             )
 from invenio.config import CFG_DEVEL_SITE
 from .models import (Workflow,
                      BibWorkflowObject,
                      BibWorkflowEngineLog,
-                     ObjectVersion)
+                     ObjectVersion,
+                     )
 from .utils import (dictproperty,
-                    get_workflow_definition)
+                    get_workflow_definition,
+                    )
 from .logger import (get_logger,
-                     BibWorkflowLogHandler)
+                     BibWorkflowLogHandler,
+                     )
 from .errors import WorkflowHalt
 
 DEBUG = CFG_DEVEL_SITE > 0
