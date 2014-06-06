5a352b53050ed2f2a48cf6dc082b4f941603d970
Jan Aage Lavik <jan.age.lavik@cern.ch>
2014-06-06 18:46:31 +0200

    workflows: WIP actions refactoring

--- invenio/modules/workflows/actions/approval.py
+++ invenio/modules/workflows/actions/approval.py
@@ -17,131 +17,54 @@
 ## along with Invenio; if not, write to the Free Software Foundation, Inc.,
 ## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
-"""Approval action."""
+"""Generic Approval action."""
 
-from wtforms import SubmitField, Form
 from invenio.base.i18n import _
-
-from wtforms.widgets import html_params, HTMLString
-
-
-def bootstrap_accept(field):
-    """
-    Accept button for hp
-    """
-    html = u'<input %s >' \
-           % html_params(id="submitButton",
-                         class_="btn btn-success",
-                         name="submitButton",
-                         type="submit",
-                         value=field.label.text,)
-    return HTMLString(u''.join(html))
+from flask import render_template
 
 
-def bootstrap_submit(field):
-    """
-    Submit button for edit record action
-    """
-    html = u'<input %s >' \
-           % html_params(id="submitButton",
-                         class_="btn btn-sm btn-primary",
-                         name="submitButton",
-                         type="submit",)
-    return HTMLString(u''.join(html))
-
-
-def bootstrap_accept_mini(field, **kwargs):
-    """
-    Mini Accept button for hp
-    """
-    objectid = kwargs.pop('objectid', '')
-    html = u'<input %s >' \
-           % html_params(id="submitButtonMini",
-                         class_="btn btn-success btn-xs",
-                         name="submitButton",
-                         type="submit",
-                         value=field.label.text,
-                         onclick="approval.mini_approval('Accept', event, %s);" % (objectid,),)
-    return HTMLString(u''.join(html))
-
-
-def bootstrap_reject(field):
-    """
-    Reject button for hp
-    """
-    html = u'<input %s >' \
-           % html_params(id="submitButton",
-                         class_="btn btn-danger",
-                         name="submitButton",
-                         type="submit",
-                         value=field.label.text,)
-    return HTMLString(u''.join(html))
-
-
-def bootstrap_reject_mini(field, **kwargs):
-    """
-    Mini Reject button for hp
-    """
-    objectid = kwargs.pop('objectid', '')
-    html = u'<input %s >' \
-           % html_params(id="submitButtonMini",
-                         class_="btn btn-danger btn-xs",
-                         name="submitButton",
-                         type="submit",
-                         value=field.label.text,
-                         onclick="approval.mini_approval('Reject', event, %s);" % (objectid,),)
-    return HTMLString(u''.join(html))
-
-
-class approval(Form):
-
+class approval(object):
     """Class representing the approval action."""
 
-    reject = SubmitField(label=_('Reject'), widget=bootstrap_reject)
-    accept = SubmitField(label=_('Accept'), widget=bootstrap_accept)
+    name = _("Approve")
+    static = ["js/workflows/actions/approval.js"]
 
-    class mini_action(Form):
-
-        """Class representing the minimal form of the approval action."""
-
-        reject = SubmitField(label=_('Reject'), widget=bootstrap_reject_mini)
-        accept = SubmitField(label=_('Accept'), widget=bootstrap_accept_mini)
-
-    def render(self, bwobject_list, bwparent_list, info_list, logtext_list,
-               w_metadata_list, workflow_func_list, *args, **kwargs):
+    def render_mini(self, obj):
         """Method to render the action."""
-        data_preview_list = []
-        # setting up approval action
-        for bwo in bwobject_list:
-            data_preview_list.append(bwo.get_formatted_data())
+        return render_template(
+            'workflows/action/approve_mini.html',
+            {
+                'message': obj.get_message(),
+                'object_id': obj.id
+            }
+        )
 
-        return ('workflows/hp_approval_widget.html',
-                {'bwobject_list': bwobject_list,
-                 'bwparent_list': bwparent_list,
-                 'action': self,
-                 'data_preview_list': data_preview_list,
-                 'obj_number': len(bwobject_list),
-                 'info_list': info_list,
-                 'logtext_list': logtext_list,
-                 'w_metadata_list': w_metadata_list,
-                 'workflow_func_list': workflow_func_list})
+    def render(self, obj):
+        """Method to render the action."""
+        return {
+            "main": render_template('workflows/action/approval.html',
+                                    {
+                                        'message': obj.get_message(),
+                                        'object_id': obj.id,
+                                    })
+        }
 
-    def run(self, objectid):
+    def resolve(self, request):
         """Resolve the action taken in the approval action."""
-        from flask import request, flash
+        from flask import flash
         from ..api import continue_oid_delayed
         from ..models import BibWorkflowObject
 
-        bwobject = BibWorkflowObject.query.get(objectid)
+        object_id = request.get("object_id", None)
 
-        if request.form['decision'] == 'Accept':
-            bwobject.remove_action()
-            continue_oid_delayed(objectid)
-            flash('Record Accepted')
+        if object_id:
+            bwobject = BibWorkflowObject.query.get(object_id)
 
-        elif request.form['decision'] == 'Reject':
-            BibWorkflowObject.delete(objectid)
-            flash('Record Rejected')
+            if request.form['decision'] == 'Accept':
+                bwobject.remove_action()
+                continue_oid_delayed(object_id)
+                flash('Record Accepted')
 
-approval.name = "Approve this record"
-approval.static = ["js/workflows/actions/approval.js"]
+            elif request.form['decision'] == 'Reject':
+                BibWorkflowObject.delete(object_id)
+                flash('Record Rejected')
