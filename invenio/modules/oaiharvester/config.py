5e0f363013a7715f73c904afa1eaa780bd8377f0
Jan Aage Lavik <jan.age.lavik@cern.ch>
2015-04-30 11:00:11 +0200

    oaiharvester: storage directory addition
    * Adds a new config variable OAIHARVESTER_STORAGEDIR that will be
      used in workflows to store intermediate files.
    
    * Updates some workflow tasks to use this new location.
    
    Signed-off-by: Jan Aage Lavik <jan.age.lavik@cern.ch>

--- invenio/modules/oaiharvester/config.py
+++ invenio/modules/oaiharvester/config.py
@@ -19,8 +19,17 @@
 
 """OAI harvest config."""
 
+
 from __future__ import unicode_literals
+
+import os
+import sys
+
 
 OAIHARVESTER_DEFAULT_NAMESPACE_MAP = {
     None: "http://www.openarchives.org/OAI/2.0/",
 }
+"""The default namespace used when handling OAI-PMH results."""
+
+OAIHARVESTER_STORAGEDIR = os.path.join(sys.prefix, "var/data/oaiharvester/storage")
+"""Path to a storage directory where the oaiharvester may put files."""
